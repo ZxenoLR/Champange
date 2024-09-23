c78f4d6e30e7c5de99062c5df1ea6baf4aafe421
ZxenoLR <pdiddysex829@gmail.com>
2024-09-23 06:03:07 +0100

    Update Main.lua

--- Loader/UILib/Main.lua
+++ Loader/UILib/Main.lua
@@ -3077,7 +3077,7 @@ function textboxs:set(value)
 	self.callback(value)
 end
 --
-function sections:keybind(props)
+function sections:Keybind(props)
 	-- // properties
 	local name = props.name or props.Name or props.page or props.Page or props.pagename or props.Pagename or props.PageName or props.pageName or "new ui"
 	local def = props.def or props.Def or props.default or props.Default or nil
